2879edec1edb59cd962329b129be1e3bfa450639
Clément Caillat <clement-caillat@laplateforme.io>
2020-03-10 11:05:03 +0100

    Job 2 fini

---
+++ jour02/job03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack 3 - Jour 2 - Job 3</title>
+</head>
+<body>
+    <button id="button">Click me</button>
+    <article id="compteur"></article>
+</body>
+</html>
+<script src="script.js"></script>
