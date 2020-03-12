0b5ad778f4a676d6445d891bf95dc3d876cf51ae
Clément Caillat <clement-caillat@laplateforme.io>
2020-03-12 09:58:45 +0100

    Fini

---
+++ jour04/job01/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    $('#button').click(function(){
+        $.ajax({
+            url: "expression.txt",
+            dataType: 'text',
+            success: function(data) {
+                $('body').append('<p>' + data+ '</p>');
+            }
+        })
+    })
+})
